b91c56c7d14c4fc485b0c93c6c74f4ba7d9721ba
harrika <henry.kirya@gmail.com>
2013-07-06 16:23:42 +0000

    modified add for fsRead

--- web.js
+++ web.js
@@ -4,9 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
    var fs = require("fs");
-   var buffer = new Buffer(16);
-      buffer.write(fs.readFileSync("index.html"), "utf-8");
-     response.send(buffer.toString("utf-8");
+      response.send(fs.readFileSync("index.html"), "utf-8");
 });
 
 var port = process.env.PORT || 5000;
