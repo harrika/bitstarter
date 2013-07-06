2bf76d8fb27686f4d8328d1e2266ffb46b006108
harrika <henry.kirya@gmail.com>
2013-07-06 21:34:10 +0000

    added buffers

--- web.js
+++ web.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express.createServer(express.logger());
-app.get('/', function(request, response);
-var fs = require('fs');
-var buffer = new Buffer(fs.readFileSync("index.html"));
-response.send(buffer.toString('utf-8'));
+app.get('/', function(request, response) {
+  response.send('Hello World 2!');
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
